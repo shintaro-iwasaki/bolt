2ae97fa0cc76cae67ce157aece7f2a8931bd1fcb
Shintaro Iwasaki <siwasaki@anl.gov>
2021-07-14 16:42:30 -0500

    [BOLT] clean up examples
    - Remove outdated benchmarks
    - Add <stdlib.h> (thank @RikkiButler20 for reporting this issue!)
    - Fix the code format

--- examples/sample_nested.c
+++ examples/sample_nested.c
@@ -6,54 +6,48 @@
 
 #include <omp.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 
+int main(int argc, char *argv[]) {
 
+  int size = (argc > 1) ? atoi(argv[1]) : 100;
+  int i, j;
+  int nthreads;
+  struct timeval t_start, t_end;
+  double time;
 
-int main(int argc, char * argv[]) {
+  double *a = (double *)malloc(sizeof(double) * size * size);
 
-    int size=(argc>1)?atoi(argv[1]):100;
-    int i,j,k=0;
-    int nthreads;
-    struct timeval t_start, t_end;
-    double time;
+#pragma omp parallel
+  { nthreads = omp_get_num_threads(); }
 
-    double *a = (double *)malloc(sizeof(double)*size*size);
+  for (i = 0; i < size * size; i++) {
+    a[i] = i;
+  }
 
-    #pragma omp parallel
-    {
-        nthreads=omp_get_num_threads();
+  gettimeofday(&t_start, NULL);
+
+#pragma omp parallel for
+  for (i = 0; i < size; i++) {
+#pragma omp parallel for
+    for (j = 0; j < size; j++) {
+      a[i * size + j] = a[i * size + j] * 0.9;
     }
+  }
 
-    for(i=0;i<size*size;i++){
-        a[i]=i;
-    }
+  gettimeofday(&t_end, NULL);
 
-    gettimeofday(&t_start,NULL);
-
-    #pragma omp parallel for
-    for(i=0;i<size;i++){
-        #pragma omp parallel for
-        for(j=0;j<size;j++){
-            a[i*size+j]=a[i*size+j]*0.9;
-        }
-    }
-
-    gettimeofday(&t_end,NULL);
-
-	
-
-    time=(t_end.tv_sec * 1000000 + t_end.tv_usec) -
+  time = (t_end.tv_sec * 1000000 + t_end.tv_usec) -
          (t_start.tv_sec * 1000000 + t_start.tv_usec);
 
+  printf("%d %f\n", nthreads, time / 1000000.0);
 
-    printf("%d %f\n",nthreads,time/1000000.0);
-
-    for(i=0;i<size*size;i++){
-        if(a[i]!=i*0.9){
-	    printf("a[%d]=%f\n",i,a[i]);
-	    return 1;
-        }       
+  for (i = 0; i < size * size; i++) {
+    if (a[i] != i * 0.9) {
+      printf("a[%d]=%f\n", i, a[i]);
+      return 1;
     }
-
+  }
+  free(a);
 }
